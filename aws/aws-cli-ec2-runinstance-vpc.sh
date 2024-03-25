cffc806d5c7b707d151801fde7491c888c12b92c
eric-tekton-nelson <eric.nelson95@gmail.com>
2024-03-25 23:28:56 +0000

    new script for efs mount

--- aws/aws-cli-ec2-runinstance-vpc.sh
+++ aws/aws-cli-ec2-runinstance-vpc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Based on VPC vpc-0e47dee32a67e7219 / main-ue2-vpc-0001
+# Based on VPC main-ue2-vpc-0001
 
 # Launch instances in mue2-public-az2a
 aws ec2 run-instances --image-id <ami-image> --instance-type <type> --security-group-ids <sg-id> --subnet-id <subnet> --key-name <kms-key-id> --user-data file://<path>
