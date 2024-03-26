e1694f7fd3e2fe5fb1f1b136600afb8336614f98
eric-tekton-nelson <eric.nelson95@gmail.com>
2024-03-26 00:05:39 +0000

    some tweaks after finally getting EFS mount to work on this ubuntu instance

--- aws/aws-cli-efs-create-mount.sh
+++ aws/aws-cli-efs-create-mount.sh
@@ -15,9 +15,13 @@
 # sudo apt-get -y install amazon-efs-utils
 
 # mount using Amazon EFS mount helper (get fs id from EFS details)
+# No clue why, but this does not work, maybe issue with ubuntu
 # mkdir ~/efs-dir  # EFS mount point dir
-sudo mount -t efs -o tls fs-06a3f05c08ae04b64.efs.us-east-2.amazonaws.com:/ ~/efs-dir
-# sudo mount -t efs -o tls fs-06a3f05c08ae04b64:/ ~/efs-dir
+# sudo mount -t efs -o tls fs-06a3f05c08ae04b64.efs.us-east-2.amazonaws.com:/ ~/efs-dir
+
+# mount using nfs4 utility
+# this command can be found when selecting the "attach" button in EFS page on console
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 10.0.3.108:/ ~/efs-dir
 
 # Unmount EFS file system
 # sudo umoint ~/efs-dir
