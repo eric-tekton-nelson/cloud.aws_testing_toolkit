5a068d609eab087cc64002e5efbf5338b06e37ce
eric-tekton-nelson <eric.nelson95@gmail.com>
2024-03-26 01:52:52 +0000

    adding script to install all my fav repos

---
+++ github/git-repos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# AWS Digital Cloud Training
+git clone git@github.com:nealdct/aws-ccp-code.git
+git clone git@github.com:nealdct/aws-dva-code.git
+git clone git@github.com:nealdct/serverless-for-beginners.git
+
+# Testing toolkit
+git clone git@github.com:eric-tekton-nelson/cloud.aws_testing_toolkit.git
+
+# AWS Cloudformation Templates: Cloud9
+git clone git@github.com:aws-samples/aws-cloud9-bootstrapping-example.git
+
+# AWS Cloudforamtion Template Patterns
+git clone git@github.com:awslabs/aws-cloudformation-templates.git
+
+# AWS Lambda Ephemeral Architecture
+git clone git@github.com:aws-samples/ephemeral-preview-containers-furl.git
+
+# AWS Cloud Game Server
+git clone git@github.com:eric-tekton-nelson/valheim-on-cloud9.git
